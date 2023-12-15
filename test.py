69788eaba987f2df976498bac82bc136ffab2965
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-12-14 17:00:30 -0800

    Improved demo with hoverDat

--- test.py
+++ test.py
@@ -7,29 +7,57 @@ import plotly.graph_objects as go
 # Axes ranges
 RANGE_X = [-5,5]
 RANGE_Y = [-5,5]
+DELTA = 0.25
+
+# Sample data
+DATA = np.random.randn(100, 2)
 
 # Create a Dash application
 app = dash.Dash(__name__)
 
 def create_scatter_fig() -> go.Figure:
 
-    # Sample data
-    data = np.random.randn(100, 2)
+    fig = go.Figure()
 
     # Figure
-    fig = go.Figure()
-    fig.add_trace(
-        go.Scatter(
-            x=data[:, 0],
-            y=data[:, 1],
-            mode='markers',
-            marker=dict(size=8)
+    trace = go.Scatter(
+        x=DATA[:, 0],
+        y=DATA[:, 1],
+        mode='markers',
+        marker=dict(size=8, color='blue')
         )
-    )
+    trace.hoverinfo = 'none' # No hover info shown - callbacks still fire
+
+    fig.add_trace(trace)
+
+    trace = go.Scatter(
+        x=DATA[:, 0],
+        y=DATA[:, 1],
+        mode='markers',
+        marker=dict(size=8, color='red')
+        )
+    trace.hoverinfo = 'none' # No hover info shown - callbacks still fire
+
+    fig.add_trace(trace)
+
+    # Grid on top
+    grid = np.mgrid[RANGE_X[0]:RANGE_X[1]:DELTA, RANGE_Y[0]:RANGE_Y[1]:DELTA]
+    trace = go.Scatter(
+            x=grid[0].flatten(),
+            y=grid[1].flatten(),
+            mode='markers',
+            marker=dict(size=1, color='gray', opacity=0.0),
+            showlegend=False
+        )
+
+    # No hover info shown - callbacks still fire
+    trace.hoverinfo = 'none'
+
+    fig.add_trace(trace)
+
     fig.update_xaxes(range=RANGE_X)
     fig.update_yaxes(range=RANGE_Y)
     fig.update_layout(
-        dragmode='pan',  # Set dragmode to 'lasso' for selection
         title='Draggable Scatter Plot'
     )
 
@@ -39,27 +67,6 @@ def create_scatter_fig() -> go.Figure:
 
     # Set background color to white
     fig.update_layout(plot_bgcolor='white')
-
-    # Make image of the figure
-    fig.write_image("fig1.png")
-
-    import base64
-    img = base64.b64encode(open("fig1.png", 'rb').read())
-
-    # Add image to the figure
-    fig.add_layout_image(
-        dict(
-            source='data:image/png;base64,{}'.format(img.decode()),
-            xref="x",
-            yref="y",
-            x=RANGE_X[0],
-            y=RANGE_Y[1],
-            sizex=abs(RANGE_X[1] - RANGE_X[0]),
-            sizey=abs(RANGE_Y[1] - RANGE_Y[0]),
-            sizing="stretch",
-            opacity=0.5,
-            layer="below")
-        )
 
     return fig
 
@@ -79,11 +86,34 @@ app.layout = html.Div([
 # Define callback to handle drag events
 @app.callback(
     Output('scatter-plot', 'figure'),
-    Input('scatter-plot', 'relayoutData'),
+    Input('scatter-plot', 'hoverData'),
     Input('scatter-plot', 'figure')
 )
-def update_scatter_plot(relayoutData, fig):
-    print(relayoutData)
+def update_scatter_plot(hoverData, fig):
+    print(hoverData)
+    if hoverData is None:
+        return dash.no_update
+    if not 'points' in hoverData:
+        return dash.no_update
+    if len(hoverData['points']) == 0:
+        return dash.no_update
+    pt = hoverData['points'][0]
+
+    # Check if data or grid point
+    if not 'curveNumber' in pt:
+        return dash.no_update
+    if pt['curveNumber'] != 2:
+        return dash.no_update
+    
+    # Offset
+    offset_x = pt['x']
+    offset_y = pt['y']
+
+    # Update fig data
+    fig['data'][1]['x'] = DATA[:,0] + offset_x
+    fig['data'][1]['y'] = DATA[:,1] + offset_y
+        
+    """
     if relayoutData is None:
         return dash.no_update
 
@@ -103,6 +133,8 @@ def update_scatter_plot(relayoutData, fig):
     fig['layout']['xaxis']['range'] = RANGE_X
     fig['layout']['yaxis']['range'] = RANGE_Y
 
+    return fig
+    """
     return fig
 
 # Run the Dash app
