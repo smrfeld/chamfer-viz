6eb42672d89f398c78de9964a838f6aa44547299
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2023-12-14 17:15:01 -0800

    Added app, cleaned up data part

---
+++ app.py
@@ -0,0 +1,179 @@
+from dash import dcc, html
+import dash
+from dash.dependencies import Input, Output
+import numpy as np
+import plotly.graph_objects as go
+from scipy.spatial import KDTree
+from typing import Tuple
+
+# Axes ranges
+RANGE_X = [-7,7]
+RANGE_Y = [-7,7]
+DELTA = 0.5
+
+class Data:
+
+    def __init__(self):
+        self.data = np.random.randn(100, 2)
+        self.tree = KDTree(self.data)
+
+    @property
+    def data_x(self):
+        return self.data[:, 0]
+
+    @property
+    def data_y(self):
+        return self.data[:, 1]
+
+    def data_offset(self, offset_x: float, offset_y: float) -> np.ndarray:
+        return self.data + np.array([offset_x, offset_y])
+    
+    def data_offset_xy(self, offset_x: float, offset_y: float) -> Tuple[np.ndarray, np.ndarray]:
+        data_offset = self.data_offset(offset_x, offset_y)
+        return data_offset[:, 0], data_offset[:, 1]
+
+    def chamfer_distance(self, data_offset: np.ndarray) -> float:
+        """
+        Computes the chamfer distance between two sets of points A and B.
+        """
+        tree_offset = KDTree(data_offset)
+        dist_A = tree_offset.query(self.data)[0]
+        dist_B = self.tree.query(data_offset)[0]
+        return np.mean(dist_A) + np.mean(dist_B)
+
+    def chamfer_distance_from_offset(self, offset_x: float, offset_y: float) -> float:
+        data_offset = self.data_offset(offset_x, offset_y)
+        return self.chamfer_distance(data_offset)
+
+    def chamfer_distance_title_from_offset(self, offset_x: float, offset_y: float) -> str:
+        dist = self.chamfer_distance_from_offset(offset_x, offset_y)
+        return f'Chamfer distance: {dist:.2f}'
+
+DATA = Data()
+
+# Create a Dash application
+app = dash.Dash(__name__)
+
+def create_scatter_fig() -> go.Figure:
+
+    fig = go.Figure()
+
+    # Figure
+    trace = go.Scatter(
+        x=DATA.data_x,
+        y=DATA.data_y,
+        mode='markers',
+        marker=dict(size=8, color='blue')
+        )
+    trace.hoverinfo = 'none' # No hover info shown - callbacks still fire
+
+    fig.add_trace(trace)
+
+    trace = go.Scatter(
+        x=DATA.data_x,
+        y=DATA.data_y,
+        mode='markers',
+        marker=dict(size=8, color='red')
+        )
+    trace.hoverinfo = 'none' # No hover info shown - callbacks still fire
+
+    fig.add_trace(trace)
+
+    # Grid on top
+    grid = np.mgrid[RANGE_X[0]:RANGE_X[1]:DELTA, RANGE_Y[0]:RANGE_Y[1]:DELTA]
+    trace = go.Scatter(
+        x=grid[0].flatten(),
+        y=grid[1].flatten(),
+        mode='markers',
+        marker=dict(size=1, color='gray', opacity=0.0),
+        showlegend=False
+    )
+
+    # No hover info shown - callbacks still fire
+    trace.hoverinfo = 'none'
+
+    fig.add_trace(trace)
+
+    fig.update_xaxes(range=RANGE_X)
+    fig.update_yaxes(range=RANGE_Y)
+
+    # No legend
+    fig.update_layout(showlegend=False, font=dict(size=24))
+
+    # Square aspect ratio
+    fig.update_layout(
+        width=600,
+        height=600,
+        autosize=False,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=80,
+            pad=0
+        )
+        )
+
+    # Hide axes
+    fig.update_xaxes(showticklabels=False, showgrid=False, zeroline=False)
+    fig.update_yaxes(showticklabels=False, showgrid=False, zeroline=False)
+
+    # Title
+    fig.update_layout(title=DATA.chamfer_distance_title_from_offset(0, 0))
+
+    # Set background color to white
+    fig.update_layout(plot_bgcolor='white')
+
+    return fig
+
+fig = create_scatter_fig()
+
+# Define the layout of the app
+app.layout = html.Div([
+    dcc.Graph(
+        id='scatter-plot',
+        figure=fig,
+        config={
+           'displayModeBar': False
+        }
+    )
+])
+
+# Define callback to handle drag events
+@app.callback(
+    Output('scatter-plot', 'figure'),
+    Input('scatter-plot', 'hoverData'),
+    Input('scatter-plot', 'figure')
+)
+def update_scatter_plot(hoverData, fig):
+
+    if hoverData is None:
+        return dash.no_update
+    if not 'points' in hoverData:
+        return dash.no_update
+    if len(hoverData['points']) == 0:
+        return dash.no_update
+    pt = hoverData['points'][0]
+
+    # Check if data or grid point
+    if not 'curveNumber' in pt:
+        return dash.no_update
+    if pt['curveNumber'] != 2:
+        return dash.no_update
+    
+    # Offset
+    offset_x = pt['x']
+    offset_y = pt['y']
+
+    # Update fig data
+    fig['data'][1]['x'], fig['data'][1]['y'] = DATA.data_offset_xy(offset_x, offset_y)
+        
+    # Title
+    fig['layout']['title']['text'] = DATA.chamfer_distance_title_from_offset(offset_x, offset_y)
+
+    return fig
+
+# Run the Dash app
+if __name__ == '__main__':
+
+    app.run_server(debug=True)
